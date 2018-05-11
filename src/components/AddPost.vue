0ce02da6d3a88804e05fa74b314a84a24641d1dd
NoHeal4u <miodragnikolic@yahoo.com>
2018-05-11 19:59:13 +0200

    uradjen i peti

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -5,7 +5,7 @@ Add Post
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<form @submit.prevent="addPost">
+<form @submit.prevent="addPost" @reset= "resetForm">
   <div class="form-group row">
     <label for="text" class="col-4 col-form-label">Post Title</label> 
     <div class="col-8">
@@ -26,6 +26,8 @@ Add Post
   <div class="form-group row">
     <div class="offset-4 col-8">
       <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+      <input  type="reset" value="Reset the form">
+
     </div>
   </div>
 </form>
@@ -58,6 +60,7 @@ export default {
 	methods : {
 
   	addPost(){
+
   		posts.add(this.newPost)
     		.then((response)=>{
     			this.$router.push('/posts')
@@ -67,7 +70,16 @@ export default {
           console.log(this.posts)
     		})
 
-  		}
+  		},
+
+      resetForm() {
+        this.newPost = {
+
+          title: '',
+          text: '',
+
+        }
+      }
 
  	},
 
