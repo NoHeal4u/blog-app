7d0986a9d0ce61203281016bcce48d6279b766ea
NoHeal4u <miodragnikolic@yahoo.com>
2018-05-11 18:59:45 +0200

    uradjen drugi

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,11 +1,49 @@
 <template>
+<div>
+	
 	<p>AppPosts</p>
+
+	<div class="card" style="width: 18rem;" v-for="(post, index) in posts">
+ 
+  <div class="card-body">
+    <h5 class="card-title">{{ post.title }}</h5>
+    <p class="card-text">{{ post.text }}</p>
+    
+    
+    
+  </div>
+</div>
+</div>
+	
 </template>
 
 
+
 <script>
+
+import { posts } from '../services/Posts'
 	
 	export default {
+
+		created() {
+
+			posts.getAll()
+			.then((response)=>{
+				this.posts = response.data
+				console.log(this.posts)
+			}).catch((error)=>{
+				console.log(error)
+			})
+		},
+
+
+		data() {
+
+			return {
+
+				posts: []
+			}
+		},
 
 	}
 
